68d220077c9b5ecb41a6508e01cb65d958d0b51b
yeinalee <yeinalee@gmail.com>
2023-05-09 19:46:33 +0900

    회원정보 딕셔너리

--- session/week04/python.py
+++ session/week04/python.py
@@ -3,15 +3,16 @@ print("회원가입")
 print("================================")
 
 register = False
-user={}
-
+users = []
 while not register:
+
     print("회원가입을 진행하시겠습니까?")
     print("y : 진행      n : 취소")
     answer= input(">>   ")
     answer = answer.lower()
 
     if answer == 'y':
+        user={}
         register = True
         print("================================")
         print("회원가입이 진행됩니다.")
@@ -32,6 +33,26 @@ while not register:
                 print("생년월일을 6자리로 다시 입력해주세요")
         email = input("이메일 : ")
         print("================================")
+        user['username'] = username
+        user['password'] = pwd
+        user["name"] = name
+        user["birth"] = birth
+        user["email"] = email
+        users.append(user)
+        print(users)
+        print("================================")
+        print(user["name"], "님 가입을 환영합니다!")
+
+        print("회원가입을 추가로 진행하시겠습니까?")
+        print("y : 진행      n : 취소")
+        answer = input(">>   ")
+        answer = answer.lower()
+
+        if answer == 'y':
+            pass
+        else :
+            exit()
+
     elif answer == 'n':
         print("================================")
         print("회원가입이 취소됩니다.")
