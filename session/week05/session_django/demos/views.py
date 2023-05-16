8a151d6d83a66d1c2eab2375ebef4b0bb2e07acb
Yeinalee <yeinalee@gmail.com>
2023-05-16 20:39:12 +0900

    DJango session finish

--- session/week05/session_django/demos/views.py
+++ session/week05/session_django/demos/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def helloBabyLion(rerquest): #request 받아서
-    return HttpResponse("Hello DJango")
+def helloBabyLion(request): #request 받아서
+    return render(request, 'crazyDjango.html')
