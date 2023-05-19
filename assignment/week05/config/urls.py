e516dc112db7557d768b7ad168e391ce45d07539
Yeinalee <yeinalee@gmail.com>
2023-05-19 23:43:13 +0900

    html, css 추가

--- assignment/week05/config/urls.py
+++ assignment/week05/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demos.views import study_func
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('study/', study_func),
 ]
--- assignment/week05/demos/views.py
+++ assignment/week05/demos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def study_func(request):
+    return render(request, 'stst.html')  # stst.html 파일 렌더링
