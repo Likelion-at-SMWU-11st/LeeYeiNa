8d500cc388c8d89f23167775b4642643d6bea836
Yeinalee <yeinalee@gmail.com>
2023-06-27 20:14:30 +0900

    새로운 모델 추가

--- session/week07/posts/models.py
+++ session/week07/posts/models.py
@@ -8,3 +8,9 @@ class Post(models.Model):
     contentn = models.TextField('내용')
     created_at = models.DateTimeField('작성일')
     view_count = models.IntegerField('조회수')
+
+
+class MyPage(models.Model):
+    image = models.ImageField(verbose_name='이미지')
+    email = models.EmailField('이메일')
+    post = models.ImageField('포스트 사진')
