f9909e3e1f4479bc3c4015fa83571ea4a856cbd0
Yeinalee <yeinalee@gmail.com>
2023-07-10 23:18:28 +0900

    basic 미션 완료

--- session/week07/lotto/views.py
+++ session/week07/lotto/views.py
@@ -1,3 +1,13 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
+def lotto_basic(request):
+    lotto_num = list() #로또 번호를 저장할 리스트
+
+    #로또 번호 7자리
+    for _ in range(7): #단순 반복
+        lotto_num.append(random.randint(1, 45))
+    
+    #render : html 템플릿에 데이터(딕셔너리 형식)를 넣어 보냄
+    return render(request, 'lotto_basic.html', {'lotto_num' : lotto_num })
--- session/week07/myinsta/urls.py
+++ session/week07/myinsta/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lotto.views import lotto_basic
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('lottobasic/', lotto_basic, name = 'lotto_basic'),
 ]
+
