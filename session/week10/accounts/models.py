13206bac9976de98de81352827d4c9460f98b08a
Yeinalee <yeinalee@gmail.com>
2023-08-02 21:56:23 +0900

    models.py 수정

--- session/week10/accounts/models.py
+++ session/week10/accounts/models.py
@@ -2,17 +2,3 @@ from django.db import models
 from django.shortcuts import render, redirect
 from .forms import UserCreateForm, SignupForm
 # Create your models here.
-
-
-def signup_view(request):
-    if request.method == "GET":
-        form = UserCreateForm
-        context = {'form': form}
-        return render(request, 'accounts/signup.html', context)
-    else:
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            instance = form.save()
-            return redirect('index')
-        else:
-            return redirect('accounts:signup')
